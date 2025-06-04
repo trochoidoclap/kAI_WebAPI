042b3ca3ad09e58dd3cb955e91daf183e0a8e62b
trochoidoclap <bggamingpbsvn@gmail.com>
2025-06-04 15:57:04 +0700

    Update lại Question và Subjects

---
+++ kAI_webAPI/kAI_webAPI/Mappers/SubjectsMapper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using kAI_webAPI.Models.Subjects;
+using kAI_webAPI.Dtos.Subjects;
+namespace kAI_webAPI.Mappers
+{
+    public static class SubjectsMapper
+    {   public static SubjectsDTOs ToSubjectsDto(this Subjects subjectsModel)
+        {
+            return new SubjectsDTOs
+            {
+                Subject = subjectsModel.Subject,
+                Type = subjectsModel.Type ?? string.Empty
+            };
+        }
+        public static SubjectsGroupDTOs ToSubjectsGroupDto(this Subjects_group subjectsGroupModel)
+        {
+            var dto = new SubjectsGroupDTOs
+            {
+                Subjects = new List<SubjectsDTOs>()
+            };
+            if (subjectsGroupModel.Subjects1 != null)
+            {
+                dto.Subjects.Add(subjectsGroupModel.Subjects1.ToSubjectsDto());
+            }
+            if (subjectsGroupModel.Subjects2 != null)
+            {
+                dto.Subjects.Add(subjectsGroupModel.Subjects2.ToSubjectsDto());
+            }
+            if (subjectsGroupModel.Subjects3 != null)
+            {
+                dto.Subjects.Add(subjectsGroupModel.Subjects3.ToSubjectsDto());
+            }
+            if (subjectsGroupModel.Subjects4 != null)
+            {
+                dto.Subjects.Add(subjectsGroupModel.Subjects4.ToSubjectsDto());
+            }
+            if (subjectsGroupModel.Subjects5 != null)
+            {
+                dto.Subjects.Add(subjectsGroupModel.Subjects5.ToSubjectsDto());
+            }
+            return dto;
+        }
+    }
+}
--- kAI_webAPI/kAI_webAPI/Models/Subjects/Subjects.cs
+++ kAI_webAPI/kAI_webAPI/Models/Subjects/Subjects.cs
@@ -12,30 +12,6 @@ namespace kAI_webAPI.Models.Subjects
         [StringLength(50)]
         public string? Type { get; set; } = string.Empty;
     }
-    public class Subjects_group
-    {
-        [Key]
-        public int Id_subgroup { get; set; }
-
-        public int Subject1 { get; set; }
-        [ForeignKey(nameof(Subject1))]
-        public Subjects? Subjects1 { get; set; }
-
-        public int Subject2 { get; set; }
-        [ForeignKey(nameof(Subject2))]
-        public Subjects? Subjects2 { get; set; }
-
-        public int Subject3 { get; set; }
-        [ForeignKey(nameof(Subject3))]
-        public Subjects? Subjects3 { get; set; }
-
-        public int? Subject4 { get; set; }
-        [ForeignKey(nameof(Subject4))]
-        public Subjects? Subjects4 { get; set; }
-
-        public int? Subject5 { get; set; }
-        [ForeignKey(nameof(Subject5))]
-        public Subjects? Subjects5 { get; set; }
-    }
+    
 
 }
